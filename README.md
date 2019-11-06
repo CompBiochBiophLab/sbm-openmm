e870d3808a6d17c58e5f4e931da78e7f140c825c
Martin-Floor <martin.floor@uvi.cat>
2019-11-06 19:05:56 +0100

    Added sbm reporter

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # sbm-openmm
 
 Python library to run structure based model simulations using OpenMM toolkit
+
--- sbmOpenMM/__init__.py
+++ sbmOpenMM/__init__.py
@@ -1,3 +1,7 @@
+#Import three main classes
 from .app import geometry
 from .app import system
 from .app import models
+
+#Import special reporter
+from .app import sbmReporter
